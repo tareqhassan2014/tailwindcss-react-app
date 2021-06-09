1ca19deb03d1d5870b1369567ad8b95bfdab9f30
Tareq Hassan <tareqhassan2014@gmail.com>
2021-06-09 20:26:20 +0600

    finishing Navebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import {
+  BrowserRouter as Router,
+
+  Route, Switch
+} from "react-router-dom";
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/">
+        
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
